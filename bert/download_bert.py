c8728cd4efbd3da55a670065c05de555781725c5
thisisjustausername <131529448+thisisjustausername@users.noreply.github.com>
2026-08-01 22:23:10 +0200

    refactor: change model

--- bert/download_bert.py
+++ bert/download_bert.py
@@ -1,4 +1,5 @@
 from huggingface_hub import snapshot_download
 
 repo = 'google-bert/bert-base-german-cased'
-snapshot_download(repo, local_dir='models/bert-base-german-cased')
+repo = 'LSX-UniWue/ModernGBERT_1B'
+snapshot_download(repo, local_dir='models/modern-german-bert')
