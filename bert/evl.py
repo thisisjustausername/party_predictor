0ba5b385a10063dc0788d180f5ac5f9887b408cd
thisisjustausername <131529448+thisisjustausername@users.noreply.github.com>
2026-08-02 16:42:00 +0200

    feat: fit to new problem

--- bert/evl.py
+++ bert/evl.py
@@ -6,37 +6,31 @@ import evaluate
 
 from bert.parameters import *
 
-metric = evaluate.load('seqeval')
+f1_metric = evaluate.load('f1')
+precision_metric = evaluate.load('precision')
+recall_metric = evaluate.load('recall')
+accuracy_metric = evaluate.load('accuracy')
 
-def evaluate_model(model, data_loader, tokens_included: bool = False):
-    # Compute accuracy of model on data provided by data_loader
-    num_instances = len(data_loader.dataset)
+def evaluate_model(model, data_loader):
     labels = []
     predictions = []
-    with torch.no_grad(): # This tells the model that we're not training
-                        # Will not remember gradients for this block
+    with torch.no_grad():
         model.eval()
 
         for i in iter(data_loader):
-            if tokens_included:
-                X, y, tokens = i
-            else:
-                X, y = i
+            X, y = i
             X = {k: v.to(device) for k, v in X.items()}
             y_probs = model(X)
             y_preds = torch.argmax(y_probs, dim=-1)
-            for row_y, row_pred in zip(y, y_preds):
-                label_row = []
-                preds_row = []
-                for yo, yp in zip(row_y, row_pred):
-                    if yo != -100:
-                        # if yo.item() == yp.item() and yo.item() != 0:
-                            # print('Correct prediction:', yo.item(), yp.item())
-                            # print('Predicted label:', ner_labels[yp.item()])
-                        label_row.append(ner_labels[yo])
-                        preds_row.append(ner_labels[yp])
 
-                predictions.append(preds_row)
-                labels.append(label_row)
+            predictions.extend(y_preds.cpu().numpy().tolist())
+            labels.extend(y.numpy().tolist())
 
-    return metric.compute(references=labels, predictions=predictions), {'labels': labels, 'predictions': predictions}
+    results = {
+            'overall_f1': f1_metric.compute(references=labels, predictions=predictions, average='macro')['f1'], # type: ignore
+            'overall_precision': precision_metric.compute(references=labels, predictions=predictions, average='macro', zero_division=0)['precision'], # type: ignore
+            'overall_recall': recall_metric.compute(references=labels, predictions=predictions, average='macro', zero_division=0)['recall'], # type: ignore
+            'overall_accuracy': accuracy_metric.compute(references=labels, predictions=predictions)['accuracy'], # type: ignore
+        }
+
+    return results, {'labels': labels, 'predictions': predictions}
