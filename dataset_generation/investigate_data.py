e1bb60a56b9c91dd6a75f2b2600c4d1797e7773c
thisisjustausername <131529448+thisisjustausername@users.noreply.github.com>
2026-08-03 20:31:01 +0200

    feat: display more information

--- dataset_generation/investigate_data.py
+++ dataset_generation/investigate_data.py
@@ -1,3 +1,4 @@
+# %% Imports
 import json
 
 import pandas as pd
@@ -7,9 +8,11 @@ from matplotlib import pyplot as plt
 
 from bert import parameters as params
 
+# %% Import data
 with open('datasets/protocols_speeches_clean.json', 'r') as f:
     data = json.load(f)
 
+# %% Data overview
 lens = [i['speech_length'] for i in data]
 print(f'Number of speeches: {len(lens)}')
 print(f'min: {min(lens)}\nmax: {max(lens)}\nmean: {sum(lens)/len(lens)}\nmedian: {sorted(lens)[len(lens)//2]}')
@@ -21,10 +24,26 @@ del df_data
 
 display(df.head(15))
 
+
+# %% Data analysis
 print()
 print('Parties:')
 print(df['party'].unique().tolist())
+print()
+
 
 display(df.groupby('party').agg({'speech_length': ['mean', 'median', 'min', 'max']}).sort_values(('speech_length', 'mean'), ascending=False))
+display(df.groupby('party').sum())
+print()
+
+
+print('Number of speeches per party:')
+display(df.groupby('party').count().speech_length.sort_values(ascending=False))
+print()
+
+print('Number of speeches per party staying vs cut off:')
+display(f'{df[df['speech_length'] <= params.cutoff].speech_length.count()} vs {df[df['speech_length'] > params.cutoff].speech_length.count()}')
+
+# %% plots
 sns.histplot(data=df, x='speech_length', hue='party', multiple='stack', bins=50, palette=dict(zip(params.ner_labels, params.party_colors)))
 plt.show()
