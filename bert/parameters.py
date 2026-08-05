47fbaee43a5cc3f0b19a229e6c98b09c188c28f4
thisisjustausername <131529448+thisisjustausername@users.noreply.github.com>
2026-08-05 20:59:19 +0200

    feat: add early stopping, stats

--- bert/parameters.py
+++ bert/parameters.py
@@ -11,6 +11,7 @@ import torch
 # INITIALIZE PARAMETERS
 #########################
 
+# after what length of tokens (subword tokens) to cut off the input speech
 cutoff = 2500
 
 # model to use
@@ -20,30 +21,47 @@ mdl = 'modern-german-bert'
 seed = 42
 
 # training hyperparameters
-num_epochs = 16
+num_epochs = 64
+
+# early stopping
+early_stopping_patience: int | None = 5
+
+# NOTE: The effective batch size is the product of the batch size and the number of gradient accumulation steps.
 batch_size = 2
 accum_steps = 8
+
 learning_rate = 3e-5
 betas=(0.9,0.999)
 epsilon=1e-08
 
 print(f'Effective batch size: {batch_size * accum_steps}')
+
+
 #########################
 # OTHER PARAMETERS
 #########################
+
+# paths to dataset, model and repo
 dataset_base_path = 'datasets/'
 model_base_path = 'models/'
 repo_base_path = '/home/lpwgf/programming/party_predictor/'
 
+# parties
 ner_labels = ['CDU/CSU', 'SPD', 'BÜNDNIS 90/DIE GRÜNEN', 'Die Linke', 'AfD']
+
+# TODO: maybe remove this
 one_hot_encoded_labels = np.identity(len(ner_labels))
 def label_to_one_hot(label):
     return one_hot_encoded_labels[ner_labels.index(label)]
 
+# match label to index in ner_labels list
 def label_to_index(label):
     return ner_labels.index(label)
 
+# inialize party colors for plotting
 party_colors =  ['black', 'red', 'green', 'purple', 'blue']
+
+# set device for fast computation
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 with open(repo_base_path + 'datasets/party_scores.json', 'r') as f:
