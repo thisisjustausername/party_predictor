1b9fb2af9db9f6e06a5887cc3221a6253985b1fe
thisisjustausername <131529448+thisisjustausername@users.noreply.github.com>
2026-09-15 19:32:23 +0200

    feat: use saved datasplits

--- bert/label_test.py
+++ bert/label_test.py
@@ -6,25 +6,21 @@ import numpy as np
 import torch
 
 import bert.parameters as params
-from bert.data_set import do_all, ds_path
+from bert.data_set import ds_path, split_to_loader
 from bert.datatypes import BertClsModel
 from bert.evl import clean_eval, evaluate_model
 
-model_name = 'model_1'
+model_name = 'model_3_latest.pth'
 
 random_state = np.random.RandomState(params.seed)
 
 predictions = []
 
-_, _, test = do_all(ds_path('protocols_speeches_clean.json'), create_y=True, shuffle=False)
-
-with open(os.path.join(params.repo_base_path, f'finetuned_model_stats/{model_name}.json'), 'r') as f:
-    stats = json.load(f)
-batch_size = stats['batch_size']
-
 model = BertClsModel()
 model.load_state_dict(torch.load(os.path.join(params.repo_base_path, f'finetuned_models/{model_name}')))
 model = model.to(params.device)
+
+test = split_to_loader(ds_path('datasplits.json'), shuffle=(True, False, False))['test'] # type: ignore
 
 res, label_data = evaluate_model(model, test)
 result = clean_eval(res)
